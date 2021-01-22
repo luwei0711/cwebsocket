53046660ef3ebe97bea7dac158328f143a8cdf41
luwei0711 <liuxingyukushen@163.com>
2021-01-22 09:56:44 +0800

    Update server.c

--- src/cwebsocket/server.c
+++ src/cwebsocket/server.c
@@ -585,7 +585,7 @@ ssize_t cwebsocket_server_write_data(cwebsocket_connection *connection, const ch
 		return -1;
 	}
 
-	uint32_t header_length = 2 + (payload_len > 125 ? 2 : 0) + (payload_len > 0xffff ? 8 : 0);
+	uint32_t header_length = 2 + (payload_len > 125 ? 2 : 0) + (payload_len > 0xffff ? 6 : 0);
 	uint8_t header[header_length];
 	ssize_t bytes_written;
 
